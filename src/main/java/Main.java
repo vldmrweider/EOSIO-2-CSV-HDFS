485d9820496eb8d2e127932321f7c15663f645d4
Vladimir <vldmr.weider@gmail.com>
2019-01-24 16:49:27 +0300

    init

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,7 +6,8 @@ import com.mongodb.client.MongoDatabase;
 import org.bson.codecs.configuration.CodecRegistry;
 import org.bson.codecs.pojo.PojoCodecProvider;
 
-import java.util.Map;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
 
 import static com.mongodb.client.model.Filters.eq;
 import static java.util.Arrays.asList;
@@ -16,7 +17,18 @@ import static org.bson.codecs.pojo.Conventions.ANNOTATION_CONVENTION;
 
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws FileNotFoundException {
+
+        int blockStart, blockStop;
+        if (args.length != 2) {
+            System.out.println("blockstart blockend args expected ");
+            return;
+        }
+
+        PrintWriter outCsv = new PrintWriter(String.format("%s_%s.csv", args[0], args[1]));
+
+        blockStart = Integer.parseInt(args[0]);
+        blockStop = Integer.parseInt(args[1]);
 
         CodecRegistry pojoCodecRegistry = fromRegistries(MongoClientSettings.getDefaultCodecRegistry(),
                 fromProviders(PojoCodecProvider.builder().conventions(asList(ANNOTATION_CONVENTION)).automatic(true).build()));
@@ -26,18 +38,25 @@ public class Main {
         MongoDatabase database = mongoClient.getDatabase("node").withCodecRegistry(pojoCodecRegistry);
         MongoCollection<Block> collection = database.getCollection("blocks", Block.class);
 
-        Block block = collection.find(eq("block_num", 7448789)).first();
+        for (int i = blockStart; i <= blockStop; i++) {
+            Block block = collection.find(eq("block_num", i)).first();
+            if (block == null) continue;
+            BlockDetail blockDetail = block.getBlock();
+            for (Transactions tr : blockDetail.getTransactions()) {
+                Trx trx = tr.getTrx();
+                for (Action act : trx.getTransaction().getActions()) {
+                    outCsv.println(block.getBlock_num() + ";" + trx.getId() + ";" + tr.getCpu_usage_us() + ";" +
+                            tr.getNet_usage_words() + ";" + blockDetail.getProducer() +
+                            ";" + act.getAccount() + ";" + act.getName() + ";" +
+                            act.getData().entrySet());
 
-        BlockDetail blockDetail = block.getBlock();
-        for (Transactions tr : blockDetail.getTransactions()) {
-            Trx trx = tr.getTrx();
-            for (Action act : trx.getTransaction().getActions()) {
-                System.out.println(block.getBlock_num() + ";" + tr.getCpu_usage_us() + ";" + tr.getNet_usage_words() +
-                        ";" + blockDetail.getProducer() + ";" + act.getAccount() + ";" + act.getName() + ";" +
-                        act.getData().entrySet());
-                System.out.println(trx.getId());
+                }
             }
         }
+
+        outCsv.close();
+
+
     }
 
 
