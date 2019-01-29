29e01ec913976546ee3a533e61eb1d665107def6
Vladimir <vldmr.weider@gmail.com>
2019-01-29 12:09:29 +0300

    init

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,43 +3,68 @@ import com.mongodb.client.MongoClient;
 import com.mongodb.client.MongoClients;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
-
-import org.apache.hadoop.conf.Configuration;
-import org.apache.hadoop.fs.FileSystem;
-import org.apache.hadoop.fs.Path;
 import org.bson.codecs.configuration.CodecRegistry;
 import org.bson.codecs.pojo.PojoCodecProvider;
 
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.net.URI;
-import java.net.URISyntaxException;
+import java.util.Collections;
 
 import static com.mongodb.client.model.Filters.eq;
 import static java.lang.System.out;
-import static java.util.Arrays.asList;
 import static org.bson.codecs.configuration.CodecRegistries.fromProviders;
 import static org.bson.codecs.configuration.CodecRegistries.fromRegistries;
 import static org.bson.codecs.pojo.Conventions.ANNOTATION_CONVENTION;
 
+import org.apache.commons.cli.*;
 
 public class Main {
 
-    public static void main(String[] args) throws IOException, URISyntaxException {
+    public static void main(String[] args) throws IOException {
 
-        int blockStart, blockStop;
-        if (args.length != 2) {
-            out.println("blockstart blockend args expected ");
-            return;
+        Options options = new Options();
+
+        Option blockStart = new Option("b", "blockstart", true, "start block");
+        blockStart.setRequired(true);
+        options.addOption(blockStart);
+
+        Option blockStop = new Option("e", "blockstop", true, "stop block");
+        blockStop.setRequired(true);
+        options.addOption(blockStop);
+
+        Option sourse = new Option("s", "source", true, "source  type mongo, http");
+        sourse.setRequired(true);
+        options.addOption(sourse);
+
+        CommandLineParser parser = new BasicParser();
+        HelpFormatter formatter = new HelpFormatter();
+        CommandLine cmd = null;
+
+        try {
+            cmd = parser.parse(options, args);
+        } catch (ParseException e) {
+            System.out.println(e.getMessage());
+            formatter.printHelp("utility-name", options);
+            System.exit(1);
         }
 
-        PrintWriter outCsv = new PrintWriter(String.format("%s_%s.csv", args[0], args[1]));
+        int startblock = Integer.parseInt(cmd.getOptionValue("blockstart"));
+        int stopblock = Integer.parseInt(cmd.getOptionValue("blockstop"));
+        String sourcetype = cmd.getOptionValue("source");
 
-        blockStart = Integer.parseInt(args[0]);
-        blockStop = Integer.parseInt(args[1]);
+        PrintWriter outCsv = new PrintWriter(String.format("%s_%s.csv", startblock, stopblock));
 
+        if (sourcetype == "mongo") {
+            mongoSource(startblock, stopblock, outCsv);
+        } else {
+
+        }
+        outCsv.close();
+    }
+
+    private static void mongoSource(int blockStart, int blockStop, PrintWriter outCsv) {
         CodecRegistry pojoCodecRegistry = fromRegistries(MongoClientSettings.getDefaultCodecRegistry(),
-                fromProviders(PojoCodecProvider.builder().conventions(asList(ANNOTATION_CONVENTION)).automatic(true).build()));
+                fromProviders(PojoCodecProvider.builder().conventions(Collections.singletonList(ANNOTATION_CONVENTION)).automatic(true).build()));
 
         MongoClient mongoClient = MongoClients.create("mongodb://node:YtrgOJbzre04Ak7q@159.69.157.38:27017/?authSource=node");
 
@@ -60,8 +85,6 @@ public class Main {
                 }
             }
         }
-
-        outCsv.close();
     }
 
 }
