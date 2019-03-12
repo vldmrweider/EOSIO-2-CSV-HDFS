57c54dcd69a0ae876bfe10678cdfa59b8d1d2c33
Vladimir <vldmr.weider@gmail.com>
2019-03-12 16:45:23 +0300

    json acc

--- src/main/java/Main2.java
+++ src/main/java/Main2.java
@@ -9,7 +9,7 @@ public class Main2 {
         Gson gson = new Gson();
 
         PrintWriter outCsv = new PrintWriter("/media/vldmr/AAD247FFD247CE6F/gh/gh.csv");
-        final File folder = new File("/media/vldmr/AAD247FFD247CE6F/gh/201812");
+        final File folder = new File("/media/vldmr/AAD247FFD247CE6F/gh/201803");
         File[] listOfFiles = folder.listFiles();
 
         for (File file : listOfFiles) {
